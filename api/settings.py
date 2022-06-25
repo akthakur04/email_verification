0301c8e17fc252d279ecf258661beed93dd675f3
Akshay Thakur <akshay.181004@gmail.com>
2022-06-26 02:17:28 +0530

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -141,4 +141,4 @@ EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='testmailapi04@gmail.com'
-EMAIL_HOST_PASSWORD='ApiTestcheck1'
+EMAIL_HOST_PASSWORD='sadadadsadsad'
